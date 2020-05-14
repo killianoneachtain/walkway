a7139cb0cc9a431c67b1a91766ecd80e88a899bf
Killian O'Neachtain <killianoneachtain@hotmail.com>
2020-05-14 11:31:49 +0100

    Application of security features: OAuth and certificates for https:3443

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const ImageStore = require('./app/utils/image-store');
 const Hapi = require('@hapi/hapi');
+const fs = require('fs');
 
 require('./app/models/db');
 
@@ -9,6 +10,17 @@ const server = Hapi.server({
   port: 3000,
   host: 'localhost'
 });
+
+const secure_server= Hapi.server(
+  {
+    port: 3443,
+    tls: {
+      key: fs.readFileSync('keys/private/webserver.key'),
+      cert: fs.readFileSync('keys/webserver.crt')
+    },
+    host: 'localhost'
+  }
+)
 
 const dotenv = require('dotenv');
 
@@ -28,8 +40,15 @@ async function init() {
   await server.register(require('@hapi/inert'));
   await server.register(require('@hapi/vision'));
   await server.register(require('@hapi/cookie'));
+  await server.register(require('@hapi/bell'));
 
   server.validator(require('@hapi/joi'));
+
+  await secure_server.register(require('@hapi/inert'));
+  await secure_server.register(require('@hapi/vision'));
+  await secure_server.register(require('@hapi/cookie'));
+
+  secure_server.validator(require('@hapi/joi'));
 
   ImageStore.configure(credentials);
 
@@ -45,7 +64,19 @@ async function init() {
     isCached: false,
   });
 
-  server.auth.strategy('session', 'cookie', {
+  secure_server.views({
+    engines: {
+      hbs: require('handlebars'),
+    },
+    relativeTo: __dirname,
+    path: './app/views',
+    layoutPath: './app/views/layouts',
+    partialsPath: './app/views/partials',
+    layout: true,
+    isCached: false,
+  });
+
+  server.auth.strategy('cookie-auth', 'cookie', {
     cookie: {
       name: process.env.cookie_name,
       password: process.env.cookie_password,
@@ -54,12 +85,40 @@ async function init() {
     redirectTo: '/',
   });
 
-  server.auth.default('session');
+  secure_server.auth.strategy('session', 'cookie', {
+    cookie: {
+      name: process.env.cookie_name,
+      password: process.env.cookie_password,
+      isSecure: false
+    },
+    redirectTo: '/',
+  });
+
+  const bellAuthOptions = {
+    provider: 'github',
+    password: 'github-encryption-password-secure', // String used to encrypt cookie
+    // used during authorisation steps only
+    clientId: process.env.git_id,          // *** Replace with your app Client Id ****
+    clientSecret: process.env.git_client_secret,  // *** Replace with your app Client Secret ***
+    isSecure: false        // Should be 'true' in production software (requires HTTPS)
+  };
+
+  server.auth.strategy('github-oauth', 'bell', bellAuthOptions);
+
+  server.auth.default('cookie-auth');
+  //server.auth.default('session');
+  secure_server.auth.default('session');
 
   server.route(require('./routes'));
   server.route(require('./routes-api'));
+
+  secure_server.route(require('./routes'));
+  secure_server.route(require('./routes-api'));
+
   await server.start();
+  await secure_server.start();
   console.log(`Server running at: ${server.info.uri}`);
+  console.log(`Server running at: ${secure_server.info.uri}`);
 
 }
 
